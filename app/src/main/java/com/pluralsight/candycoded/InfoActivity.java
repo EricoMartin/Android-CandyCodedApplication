b57cb4ed89c0ffe27bb27cc25ba3c524c9e879b8
EricoMartin <martinirex@yahoo.co.uk>
2020-09-30 04:43:18 +0100

    completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -42,4 +42,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent action = new Intent(Intent.ACTION_DIAL);
+        Uri uriPhone = Uri.parse("tel:0123456789");
+        action.setData(uriPhone);
+        startActivity(action);
+    }
 }
